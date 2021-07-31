17e6b9182ba32e6ac8c98a50e7d86c81960af65a
404piano <popovpiano@gmail.cim>
2021-07-31 18:15:31 +0300

    init

---
+++ components/Footer.tsx
@@ -0,0 +1,46 @@
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from "@material-ui/core/styles";
+
+
+export default function Footer() {
+    const useStyles = makeStyles((theme) => ({
+        icon: {
+            marginRight: theme.spacing(2),
+        },
+        heroContent: {
+            backgroundColor: theme.palette.background.paper,
+            padding: theme.spacing(8, 0, 6),
+        },
+        heroButtons: {
+            marginTop: theme.spacing(4),
+        },
+        cardGrid: {
+            paddingTop: theme.spacing(8),
+            paddingBottom: theme.spacing(8),
+        },
+        card: {
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+        },
+        cardMedia: {
+            paddingTop: '56.25%', // 16:9
+        },
+        cardContent: {
+            flexGrow: 1,
+        },
+        footer: {
+            backgroundColor: theme.palette.background.paper,
+            padding: theme.spacing(6),
+        },
+    }));
+  return(  <footer>
+        <Typography variant="h6" align="center" gutterBottom>
+            Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+            Something here to give the footer a purpose!
+        </Typography>
+    </footer>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,39 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from "../components/Layout";
+import firebase from "firebase";
+import {createContext} from "react";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAQqWNemxI2srYB7arTYhfxCRpZDWNw85Y",
+    authDomain: "testmyproject-35977.firebaseapp.com",
+    projectId: "testmyproject-35977",
+    storageBucket: "testmyproject-35977.appspot.com",
+    messagingSenderId: "753864526331",
+    appId: "1:753864526331:web:f719d2ad66a2eb6170635e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const Context = createContext(null);
+
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+      <Context.Provider value={{
+          firebase,
+          firestore,
+          auth
+      }}>
+          <Layout>
+              <Component {...pageProps} />
+          </Layout>
+      </Context.Provider>
+
+  );
 }
 export default MyApp
